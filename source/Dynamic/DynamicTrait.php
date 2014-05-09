da5783605436aaf44fbac1f37ff97749a00d9e6c
Ilya S <public.ilya.s@gmail.com>
2014-05-09 23:25:00 +0700

    go grenn

--- source/Dynamic/DynamicTrait.php
+++ source/Dynamic/DynamicTrait.php
@@ -2,9 +2,26 @@
 
 trait DynamicTrait {
 
+    /**
+     * Instance of Dynamic.
+     *
+     * @var Dynamic
+     */
+    protected $dynamicInstance;
+
+    /**
+     * Get instance of Dynamic.
+     *
+     * @return Dynamic
+     */
     public function getDynamicInstance()
     {
+        if ( ! $this->dynamicInstance instanceof Dynamic)
+        {
+            $this->dynamicInstance = new Dynamic;
+        }
 
+        return $this->dynamicInstance;
     }
 
 }
