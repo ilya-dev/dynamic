6ec0ae98aa08e226b2139db4c27e89a251b0739a
Ilya S <public.ilya.s@gmail.com>
2014-05-10 15:26:17 +0700

    add example

--- examples/first.php
+++ examples/first.php
@@ -1,4 +1,23 @@
 <?php require __DIR__.'/../vendor/autoload.php';
 
+use Dynamic\DynamicTrait;
 
+class Example {
 
+    use DynamicTrait;
+
+    public function __construct()
+    {
+        $this->getDynamicInstance()->redirect('/^say(\w+)To$/', 'say');
+    }
+
+    public function say($what, $to)
+    {
+        echo ucfirst("$what, $to!"), PHP_EOL;
+    }
+
+}
+
+$example = new Example;
+
+echo $example->sayHelloTo('Jack'), $example->sayByeTo('John');
